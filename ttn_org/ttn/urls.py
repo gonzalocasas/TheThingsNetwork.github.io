aeeeabb5fe34bba504b3207e5c52947ea451ca8e
Martijn [pc delft] <martijn.vderveen@gmail.com>
2015-08-27 08:48:37 +0200

    Django project and app init

---
+++ ttn_org/ttn/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from . import views
+from django.views.generic import TemplateView, RedirectView
+
+APP = 'ttn'
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'ttn_org.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', TemplateView.as_view(template_name=APP+'/index.html'),
+        name='index'),
+]
