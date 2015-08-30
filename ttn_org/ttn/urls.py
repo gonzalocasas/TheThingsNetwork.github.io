1ed219c74f8e947d1d371a2058ddbcc64da2b8ce
Martijn [pc delft] <martijn.vderveen@gmail.com>
2015-08-30 17:28:29 +0200

    Communities: index page, basic

--- ttn_org/ttn/urls.py
+++ ttn_org/ttn/urls.py
@@ -2,15 +2,27 @@ from django.conf.urls import include, url
 from . import views
 from django.views.generic import TemplateView, RedirectView
 
-APP = 'ttn'
+APP = 'ttn/'
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ttn_org.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name=APP+'/index.html'),
+    # Main website
+    url(r'^$', TemplateView.as_view(template_name=APP+'index.html'),
         name='index'),
-    url(r'^landing/kickstarter$', TemplateView.as_view(template_name=APP+'/landing/kickstarter.html'),
+
+    # Community pages
+    url(r'^c/(?P<slug>[0-9a-zA-Z_-]+)$',
+        views.CommunityView.as_view(template_name=APP+'community/index.html'),
+        name='community'),
+    url(r'^start-a-community/(?P<search>[0-9a-zA-Z_-]+)?$',
+        TemplateView.as_view(template_name=APP+'community/start-a-community.html'),
+        name='new-community'),
+
+    # Special pages
+    url(r'^landing/kickstarter$',
+        TemplateView.as_view(template_name=APP+'landing/kickstarter.html'),
         name='kickstarter'),
 ]
--- ttn_org/ttn/views.py
+++ ttn_org/ttn/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.http import Http404
 
-# Create your views here.
+from .models import Community
+
+
+class CommunityView(TemplateView):
+
+    def get(self, request, slug, **kwargs):
+        c = Community.objects.filter(slug=slug)
+        if not c:
+            return redirect('ttn:new-community', search=slug)
+        context = self.get_context_data(**kwargs)
+        context['community'] = c[0]
+        return self.render_to_response(context)
+
